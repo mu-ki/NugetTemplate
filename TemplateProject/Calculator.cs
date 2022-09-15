ab1ac71a79f67cb43f8113540b9f446068478c1e
Mugunth <mugunthkumar99@gmail.com>
2022-09-15 09:14:58 +0530

    Nuget EXE Added

---
+++ TemplateProject/Calculator.cs
@@ -0,0 +1,22 @@
+﻿
+using RPA.Genie.Activities;
+
+namespace RPA.Genie.NugetTemplate
+{
+    public class Calculator : Activity
+    {
+        public InProperty<int> FirstInteger { get; set; }
+        public InProperty<int> SecondInteger { get; set; }
+        public OutProperty<int> Total { get; set; }
+
+        public override ExecutionResult Execute(IActivityContext activityContext)
+        {
+            var firstInt = FirstInteger.GetValue(activityContext);  //Get Value from UI
+            var secondInt = SecondInteger.GetValue(activityContext);
+            int total = firstInt + secondInt;
+            activityContext.SetPropertyValue(Total, total);  //Set value to UI
+            return ExecutionResult.Empty;
+        }
+
+    }
+}
--- pack.cmd
+++
@@ -1,2 +0,0 @@
-..\Tools\NuGet.exe pack RPA.Genie.NugetTemplate.csproj -Build -Prop Configuration=Release -OutputDirectory "../NugetPackages"
-pause
