a39aba9b952db13606a3ad775eb91ee0fce8fc09
Mugunth <mugunthkumar99@gmail.com>
2022-09-15 08:26:26 +0530

    Init Commit

---
+++ Calculator.cs
@@ -0,0 +1,21 @@
+﻿using RPA.Genie.Activities;
+
+namespace RPA.Genie.NugetTemplate
+{
+    public class Calculator : Activity
+    {
+        public InProperty<int> FirstInteger { get; set; }
+        public InProperty<int> SecondInteger { get; set; }
+        public OutProperty<int> Total { get; set; }
+
+        public override ExecutionResult Execute(IActivityContext activityContext)
+        {
+            var firstInt = FirstInteger.GetValue(activityContext);
+            var secondInt = SecondInteger.GetValue(activityContext);
+            int total = firstInt + secondInt;
+            activityContext.SetPropertyValue(Total, total);
+            return ExecutionResult.Empty;
+
+        }
+    }
+}
---
+++ pack.cmd
@@ -0,0 +1,2 @@
+..\Tools\NuGet.exe pack RPA.Genie.NugetTemplate.csproj -Build -Prop Configuration=Release -OutputDirectory "../NugetPackages"
+pause
